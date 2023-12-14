40f0eb96fb88351b151c5f0da26e089682e872db
adebabs-tec <ruqoyatomotayo@gmail.com>
2023-12-14 22:57:16 +0100

    hero section done

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import './App.css'
 
 import Navbar from './components/Navbar'
 import './App.css'
+import Home from './components/Home'
+import Services from './components/Services'
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Home />
+      <Services />
     </div>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,54 @@
 import { Carousel } from 'flowbite-react'
+import banner1 from '../assets/Illustration.png'
 
 const Home = () => {
   return (
     <div className="bg-neutralSilver">
       <div className="px-4 lg:px-14 max-w-screen-2xl mx-auto min-h-screen h-screen">
         <Carousel>
-          <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-            Slide 1
+          <div className="my-28 md:my-8 py-12 flex flex-col md:flex-row-reverse justify-between items-center gap-12">
+            <div>
+              <img src={banner1} alt="" />
+            </div>
+            {/* hero text  */}
+            <div className="md:w-1/2">
+              <h1 className="text-5xl font-semibold mb-4 font-neutralDGrey md:w-3/4 leading-snug">
+                Lessons and insights
+                <span className="text-brandPrimary leading-snug">
+                  {' '}
+                  from 8 years
+                </span>
+              </h1>
+              <p className="neutralGrey text-base mb-8">
+                Where to grow your business as a photographer: site or social
+                media?
+              </p>
+              <button className="py-2 px-7 text-white rounded bg-brandPrimary hover:bg-neutralDGrey transition-all duration-300 hover:-translate-y-4">
+                Register
+              </button>
+            </div>
           </div>
-          <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-            Slide 2
-          </div>
-          <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-            Slide 3
+          <div className="my-28 md:my-8 py-12 flex flex-col md:flex-row-reverse justify-between items-center gap-12">
+            <div>
+              <img src={banner1} alt="" />
+            </div>
+            {/* hero text  */}
+            <div className="md:w-1/2">
+              <h1 className="text-5xl font-semibold mb-4 font-neutralDGrey md:w-3/4 leading-snug">
+                Learn and Earn Money
+                <span className="text-brandPrimary leading-snug">
+                  {' '}
+                  in 4 months
+                </span>
+              </h1>
+              <p className="neutralGrey text-base mb-8">
+                Where to grow your business as a photographer: site or social
+                media?
+              </p>
+              <button className="py-2 px-7 text-white rounded bg-brandPrimary hover:bg-neutralDGrey transition-all duration-300 hover:-translate-y-4">
+                Register
+              </button>
+            </div>
           </div>
         </Carousel>
       </div>

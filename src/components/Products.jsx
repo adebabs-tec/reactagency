f0e4306fd516c2ca0b76810a5412fab807b750ef
adebabs-tec <ruqoyatomotayo@gmail.com>
2023-12-17 21:17:26 +0100

    new commit

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -27,6 +27,65 @@ const Products = () => {
           </button>
         </div>
       </div>
+      <div className="px-4 lg:px-14 max-w-screen-2xl mx-auto py-16 bg-neutralSilver">
+        <div className="flex flex-col md:flex-row justify-between items-center gap-8">
+          <div className="md:w-1/3">
+            <h2 className="text-4xl text-neutralDGrey font-semibold mb-4 md:w-2/3">
+              Helping a
+              <br />
+              <span className="text-brandPrimary">
+                business reinvent itself
+              </span>
+            </h2>
+            <p className="md:w-3/4 text-sm text-neutralGrey mb-8">
+              We reached here with our hard work and dedication
+            </p>
+          </div>
+          {/* stats  */}
+          <div className="md:w-1/2 mx-auto flex sm:flex-row flex-col sm:items-center justify-around gap-12">
+            <div className="space-y-8">
+              <div className="flex items-center gap-4">
+                <img src="/src/assets/icons/member.png" alt="" />
+                <div>
+                  <h4 className="text-2xl text-neutralDGrey font-semibold">
+                    2,245,341
+                  </h4>
+                  <p>Members</p>
+                </div>
+              </div>
+              <div className="flex items-center gap-4">
+                <img src="/src/assets/icons/event.png" alt="" />
+                <div>
+                  <h4 className="text-2xl text-neutralDGrey font-semibold">
+                    828,867
+                  </h4>
+                  <p>Event Bookings</p>
+                </div>
+              </div>
+            </div>
+            <div className="space-y-8">
+              <div className="flex items-center gap-4">
+                <img src="/src/assets/icons/membershipPry.png" alt="" />
+                <div>
+                  <h4 className="text-2xl text-neutralDGrey font-semibold">
+                    46,328
+                  </h4>
+                  <p>Clubs</p>
+                </div>
+              </div>
+              <div className="flex items-center gap-4">
+                <img src="/src/assets/icons/payment.png" alt="" />
+                <div>
+                  <h4 className="text-2xl text-neutralDGrey font-semibold">
+                    1,926,436
+                  </h4>
+                  <p>Payments</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }

ecef397feea4942769e2c5951f2df1441c231013
adebabs-tec <ruqoyatomotayo@gmail.com>
2023-12-23 14:03:30 +0100

    adjust products section

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,32 +3,34 @@ import Maecenas from '../assets/maecenas.png'
 
 const Products = () => {
   return (
-    <div className="px-4 lg:px-14 max-w-screen-2xl mx-auto my-8">
-      <div className="md:w-11/12 mx-auto flex flex-col md:flex-row justify-between items-center gap-12">
-        <div>
-          <img src={AboutImg} alt="" />
-        </div>
-        <div className="md:w-3/5 mx-auto">
-          <h2 className="text-4xl text-neutralDGrey font-semibold mb-4 md:w-4/5">
-            How to design your site footer like we did
-          </h2>
-          <p className="md:w-3/4 text-sm text-neutralGrey mb-8">
-            Donec a eros justo. Fusce egestas tristique ultrices. Nam tempor,
-            augue nec tincidunt molestie, massa nunc varius arcu, at scelerisque
-            elit erat a magna. Donec quis erat at libero ultrices mollis. In hac
-            habitasse platea dictumst. Vivamus vehicula leo dui, at porta nisi
-            facilisis finibus. In euismod augue vitae nisi ultricies, non
-            aliquet urna tincidunt. Integer in nisi eget nulla commodo faucibus
-            efficitur quis massa. Praesent felis est, finibus et nisi ac,
-            hendrerit venenatis libero. Donec consectetur faucibus ipsum id
-            gravida.
-          </p>
-          <button className="py-2 px-7 text-white rounded bg-brandPrimary hover:bg-neutralDGrey transition-all duration-300 hover:-translate-y-4">
-            Learn More
-          </button>
+    <>
+      <div className="px-4 lg:px-14 max-w-screen-2xl mx-auto my-8">
+        <div className="md:w-11/12 mx-auto flex flex-col md:flex-row justify-between items-center gap-12">
+          <div>
+            <img src={AboutImg} alt="" />
+          </div>
+          <div className="md:w-3/5 mx-auto">
+            <h2 className="text-4xl text-neutralDGrey font-semibold mb-4 md:w-4/5">
+              How to design your site footer like we did
+            </h2>
+            <p className="md:w-3/4 text-sm text-neutralGrey mb-8">
+              Donec a eros justo. Fusce egestas tristique ultrices. Nam tempor,
+              augue nec tincidunt molestie, massa nunc varius arcu, at
+              scelerisque elit erat a magna. Donec quis erat at libero ultrices
+              mollis. In hac habitasse platea dictumst. Vivamus vehicula leo
+              dui, at porta nisi facilisis finibus. In euismod augue vitae nisi
+              ultricies, non aliquet urna tincidunt. Integer in nisi eget nulla
+              commodo faucibus efficitur quis massa. Praesent felis est, finibus
+              et nisi ac, hendrerit venenatis libero. Donec consectetur faucibus
+              ipsum id gravida.
+            </p>
+            <button className="py-2 px-7 text-white rounded bg-brandPrimary hover:bg-neutralDGrey transition-all duration-300 hover:-translate-y-4">
+              Learn More
+            </button>
+          </div>
         </div>
       </div>
-      <div className="px-4 lg:px-14 max-w-screen-2xl mx-auto py-16 bg-neutralSilver my-8">
+      <div className="px-4 lg:px-14 max-w-screen-2xl mx-auto bg-neutralSilver py-16">
         <div className="flex flex-col md:flex-row justify-between items-center gap-8">
           <div className="md:w-1/3">
             <img src={Maecenas} alt="" />
@@ -114,7 +116,7 @@ const Products = () => {
           </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
